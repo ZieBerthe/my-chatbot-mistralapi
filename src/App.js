0b6a777a8dbba120b91dff878534d43d3f87f6f2
Aziz Berthé <zie.berthe@polytechnique.edu>
2025-03-07 00:31:58 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ConversationHeader, ChatContainer, MessageList, Message, MessageInput } from '@chatscope/chat-ui-kit-react';
+import React, { useState } from 'react';
+import { Mistral } from '@mistralai/mistralai';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const apiKey ='your-api-key';
+
+    const [messages, setMessages] = useState([
+        {
+            message: 'Hello, I am an AI, you can click on the attach button to empty the chat',
+            direction: 'incoming',
+            sender: 'chatty'
+        },
+    ]);
+
+    function vide() {
+        setMessages([{
+            message: 'Hello, I am an AI',
+            direction: 'incoming',
+            sender: 'chatty'
+        },]);
+    }
+
+    const sendmessage = async (message, isUser) => {
+        let senderType = isUser ? "outgoing" : "incoming";
+
+        const Newmessage = {
+            message: message,
+            direction: senderType
+        }
+        setMessages((prevMessages) => [...prevMessages, Newmessage]);
+
+        if (isUser) {
+            const client = new Mistral({ apiKey: apiKey });
+            const chatResponse = await client.chat.complete({
+                model: "mistral-small-2402",
+                messages: [{ role: 'user', content: message }]
+            });
+
+            sendmessage(chatResponse.choices[0].message.content, false);
+        }
+    };
+
+    return (
+        <MainContainer>
+            <ChatContainer>
+                <ConversationHeader>
+                    <ConversationHeader.Back />
+                    <ConversationHeader.Content
+                        info="Always active"
+                        userName="AI" />
+                </ConversationHeader>
+                <MessageList>
+                    {messages.map((msg, index) => (
+                        <Message key={index} model={msg} />))}
+                </MessageList>
+                <MessageInput placeholder="chat with the ai" sendButton={true} onSend={(message) => sendmessage(message, true)} onAttachClick={vide} />
+            </ChatContainer>
+        </MainContainer>
+    );
 }
 
 export default App;
